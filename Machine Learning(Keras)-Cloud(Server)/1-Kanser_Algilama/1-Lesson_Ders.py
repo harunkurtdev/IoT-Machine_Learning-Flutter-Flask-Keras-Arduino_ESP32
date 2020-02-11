bd8c1b27b12166f5e3a1b9a0ad5753b6807bf9ba
harunlakodla <50794236+harunlakodla@users.noreply.github.com>
2020-02-11 23:12:26 +0300

    Create Untitled-checkpoint.ipynb

---
+++ Machine Learning(Keras)-Cloud(Server)/1-Kanser_Algilama/1-Lesson_Ders.py
@@ -0,0 +1,45 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+import keras
+from keras.layers import Input, Dense
+from keras.optimizers import SGD
+
+from sklearn.impute import SimpleImputer as Imputer
+import numpy as np
+import pandas as pd
+
+veri = pd.read_csv("breast-cancer-wisconsin.data")
+
+veri.replace('?', -99999, inplace=True)
+#veri.drop(['id'], axis=1)
+veriyeni = veri.drop(['1000025'],axis=1)
+
+imp = Imputer(missing_values=np.nan, strategy="mean")
+veriyeni = imp.fit_transform(veriyeni)
+
+
+giris = veriyeni[:,0:8]
+cikis = veriyeni[:,9]
+
+model=Sequential()
+"""modelimizdir biz bu model üzerine eklemeler yapmaktayız her .add() dediğimiz de 
+hidden layerlarımız eklemenmektedir ancak"""
+model.add(Dense(256,input_dim=8))
+'''input_dim dediğmiz girişte ne kadar bilgimiz var ise onu belirtiyoruz 
+    256 dediğimiz ise hidden layerlara bağlantı sayılarıdır...
+    '''
+model.add(Activation("relu"))
+model.add(Dropout(0.5))
+"""Activiation() fonksiyonları ise bizim hesaplama fonklarımızdır eğer ki bu nerual
+    networklardan """
+model.add(Dense(256))
+model.add(Activation("relu"))
+model.add(Dropout(0.6))
+model.add(Dense(256))
+model.add(Activation("softmax"))
+
+model.compile(optimizer='adam', loss = 'sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(giris,cikis, nb_epoch=50,batch_size=32,validation_split=0.13)
+
+tahmin = np.array([5,5,5,8,10,8,7,3]).reshape(1,8)
+print(model.predict_classes(tahmin))
